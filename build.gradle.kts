1b0fa11104b1debde089c61adaaca8f33c5f8b28
luisrosales19 <luis.rosales@accenture.com>
2024-03-18 13:31:54 -0500

    added tests, not yet working

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	testImplementation("io.rest-assured:spring-mock-mvc")
 	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
--- src/test/kotlin/com/example/apiluis/springkotlin/SpringKotlinApplicationTests.kt
+++ src/test/kotlin/com/example/apiluis/springkotlin/SpringKotlinApplicationTests.kt
@@ -2,12 +2,38 @@ package com.example.apiluis.springkotlin
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.beans.factory.annotation.Autowired
+import kotlin.collections.isNotEmpty
+
 
 @SpringBootTest
-class SpringKotlinApplicationTests {
+class WrestlerRepositoryTests {
+
+	@Autowired
+	lateinit var wrestlerRepository: WrestlerRepository
 
 	@Test
-	fun contextLoads() {
+	fun `test findAllByOrderByCreatedAtDesc`() {
+		val wrestlers = wrestlerRepository.findAllByOrderByCreatedAtDesc()
+		assert(wrestlers.isNotEmpty()) { "List of wrestlers should not be empty" }
 	}
 
+	@Test
+	fun `test findBySlug with existing slug`() {
+		val existingSlug = "existing-slug"
+		val wrestler = Wrestlers(id = 1, name = "Test Wrestler", city = "Test City", promotion = "Test Promotion", slug = existingSlug)
+		wrestlerRepository.save(wrestler)
+
+		val foundWrestler = wrestlerRepository.findBySlug(existingSlug)
+		assert(foundWrestler.isPresent) { "Wrestler with slug $existingSlug should be found" }
+		assert(foundWrestler.get().name == "Test Wrestler") { "Found wrestler's name should be a match" }
+	}
+
+	@Test
+	fun `test findBySlug with non-existing slug`() {
+		val nonExistingSlug = "non-existing-slug"
+		val foundWrestler = wrestlerRepository.findBySlug(nonExistingSlug)
+		assert(foundWrestler.isEmpty) { "No wrestler should be found for slug $nonExistingSlug" }
+	}
 }
+
